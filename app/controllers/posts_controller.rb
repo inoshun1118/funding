c7c5f57e3c298d651cf19006e578c2db1963823d
inoshun1118 <120561118@ccalumni.meijo-u.ac.jp>
2020-05-30 19:49:12 +0900

    gage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,8 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @total_money = Backer.where(post_id: @post.id).sum(:money_value)
-    @supporters = Backer.where(post_id: @post.id).count(:money_value)
     @proportion = (@total_money.to_f / Post.where(id: @post.id).sum(:target_value).to_f) * 100
+    @supporters = Backer.where(post_id: @post.id).count(:money_value)
   end
 
   def create
