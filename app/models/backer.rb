9be9edd1aff7760279bc69d1798df1c25da6c999
inoshun1118 <120561118@ccalumni.meijo-u.ac.jp>
2020-06-15 19:18:42 +0900

    バリデーション修正

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -1,5 +1,5 @@
 class Backer < ApplicationRecord
-  validates :address, :money_value, :use_id, :post_id, presence: true
+  validates :address, :money_value, :user_id, :post_id, presence: true
   belongs_to :user
   belongs_to :post
 end
